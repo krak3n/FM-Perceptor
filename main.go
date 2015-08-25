a06c688eae549d4f9786843b234e8e08c42b4401
Chris Reeves <chris@thisissoon.com>
2015-08-25 17:57:12 +0100

    simple main.go and development documentation

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"log"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+type connection struct {
+	ws *websocket.Conn
+
+	send chan []byte
+}
+
+func serve(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	_ = &connection{send: make(chan []byte, 256), ws: ws}
+}
 
 func main() {
-	fmt.Println("foo")
+	http.HandleFunc("/", serve)
+	err := http.ListenAndServe(":9000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
