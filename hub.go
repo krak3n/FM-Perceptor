e863a114567cb90b4b314199f387fba611e6d164
Chris Reeves <chris@thisissoon.com>
2015-08-26 08:37:23 +0100

    More logging

--- hub.go
+++ hub.go
@@ -2,7 +2,7 @@
 
 package main
 
-import "github.com/Sirupsen/logrus"
+import log "github.com/Sirupsen/logrus"
 
 // Handles sending messages to all connected clients
 type hub struct {
@@ -22,14 +22,16 @@ type hub struct {
 // Listens on the channels for messages and performs the
 // relivant functionality
 func (h *hub) run() {
-	logrus.Info("Starting Hub")
+	log.Info("Starting Hub")
 	for {
 		select {
 		// On register messages, store the connection
 		case c := <-h.register:
+			log.Debug("Client Registered: ", c.ws.RemoteAddr())
 			h.connections[c] = true
 		// On unregister messsages, delete the connection from the pool
 		case c := <-h.unregister:
+			log.Debug("Client Unregistered: ", c.ws.RemoteAddr())
 			if _, ok := h.connections[c]; ok {
 				delete(h.connections, c)
 				close(c.send)
@@ -37,10 +39,12 @@ func (h *hub) run() {
 		// On incoming messages, loop over connected clients and send
 		// the message
 		case m := <-h.broadcast:
+			log.Debug("Broadcast Message: ", string(m[:]))
 			for c := range h.connections {
 				select {
 				case c.send <- m:
 				default:
+					log.Debug("Client no longer active: ", c.ws.RemoteAddr())
 					close(c.send)
 					delete(h.connections, c)
 				}
