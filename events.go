5d3b0c7c7193ab5f4c6aa0d8070c62a4b52b3158
Chris Reeves <chris@thisissoon.com>
2015-09-01 12:45:28 +0100

    Publish play event to Redis

--- events.go
+++ events.go
@@ -15,12 +15,20 @@ import (
 const (
 	CURRENT_KEY    string = "fm:player:current"
 	START_TIME_KEY string = "fm:player:start_time"
+	PLAY_EVENT     string = "play"
+	EVENT_KEY      string = "fm:events"
 )
 
 type playEvent struct {
 	Start string `json:"start" valid:"iso8601,required"`
 	Uri   string `json:"uri" valid:"required"`
 	User  string `json:"user" valid:"required"`
+}
+
+type PublishEvent struct {
+	Event string `json:"event"`
+	Uri   string `json:"uri"`
+	User  string `json:"user"`
 }
 
 // Handle sending a play event (POST /events/play)
@@ -55,6 +63,24 @@ func playHandler(c web.C, w http.ResponseWriter, r *http.Request) {
 
 	// Save Start Time
 	err = red.Set(START_TIME_KEY, string(event.Start), 0).Err()
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	// Publish
+	message, err := json.Marshal(&PublishEvent{
+		Event: PLAY_EVENT,
+		Uri:   event.Uri,
+		User:  event.User,
+	})
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	// Publish Message
+	err = red.Publish(EVENT_KEY, string(message[:])).Err()
 	if err != nil {
 		http.Error(w, http.StatusText(500), 500)
 		return
