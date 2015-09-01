e424a9b078638e4f1e4e75b035ad869eb89d00e4
Chris Reeves <chris@thisissoon.com>
2015-09-01 14:28:46 +0100

    Handle POST /events/end

--- events.go
+++ events.go
@@ -19,6 +19,11 @@ type playEvent struct {
 	User  string `json:"user" valid:"required"`
 }
 
+type endEvent struct {
+	Track string `json:"uri" valid:"required"`
+	User  string `json:"user" valid:"required"`
+}
+
 // Handle sending a play event (POST /events/play)
 func playHandler(c web.C, w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
@@ -57,7 +62,37 @@ func playHandler(c web.C, w http.ResponseWriter, r *http.Request) {
 
 // Handle sending a end event (POST /events/end)
 func endHandler(c web.C, w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("end"))
+	decoder := json.NewDecoder(r.Body)
+	event := &endEvent{}
+
+	// Decode JSON
+	err := decoder.Decode(&event)
+	if err != nil {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+
+	// Validate
+	res, err := v.ValidateStruct(event)
+	if err != nil {
+		fmt.Println(err.Error())
+		http.Error(w, http.StatusText(422), 422)
+		fmt.Println(res)
+		return
+	}
+
+	// Publish event
+	if err := events.PublishEndEvent(
+		c.Env["REDIS"].(*redis.Client),
+		event.Track,
+		event.User); err != nil {
+
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	// We got to the end - everything went fine!
+	w.WriteHeader(201)
 }
 
 // Handle sendiong a volume change event (POST /events/volume)
